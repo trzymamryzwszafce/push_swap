dc1a4c852f2a8e76ab138d2be2a5f56ec56139b4
Szymon <szmadeja@localhost.localdomain>
2025-06-10 00:08:08 +0200

    sdf

--- moves.c
+++ moves.c
@@ -76,3 +76,85 @@ void	pb(t_stack *stack)
 	stack->size_a--;
 	ft_printf("pb\n");
 }
+
+void	ra(t_stack *stack, bool check)
+{
+	int	i;
+	int	temp;
+
+	i = 0;
+	temp = stack->stack_a[0];
+	while (i < stack->size_a - 1)
+	{
+		stack->stack_a[i] = stack->stack_a[i + 1];
+		i++;
+	}
+	stack->stack_a[i] = temp;
+	if (check)
+		ft_printf("ra\n");
+}
+
+void	rb(t_stack *stack, bool check)
+{
+	int	i;
+	int	temp;
+
+	i = 0;
+	temp = stack->stack_b[0];
+	while (i < stack->size_b - 1)
+	{
+		stack->stack_b[i] = stack->stack_b[i + 1];
+		i++;
+	}
+	stack->stack_b[i] = temp;
+	if (check)
+		ft_printf("rb\n");
+}
+
+void	rr(t_stack *stack)
+{
+	ra(stack, false);
+	rb(stack, false);
+	ft_printf("rr\n");
+}
+
+void	rra(t_stack *stack, bool check)
+{
+	int	i;
+	int	temp;
+
+	i = stack->size_a - 1;
+	temp = stack->stack_a[i];
+	while (i > 0)
+	{
+		stack->stack_a[i] = stack->stack_a[i - 1];
+		i--;
+	}
+	stack->stack_a[i] = temp;
+	if (check)
+		ft_printf("rra\n");
+}
+
+void	rrb(t_stack *stack, bool check)
+{
+	int	i;
+	int	temp;
+
+	i = stack->size_b - 1;
+	temp = stack->stack_b[i];
+	while (i > 0)
+	{
+		stack->stack_b[i] = stack->stack_b[i - 1];
+		i--;
+	}
+	stack->stack_b[i] = temp;
+	if (check)
+		ft_printf("rra\n");
+}
+
+void	rrr(t_stack *stack)
+{
+	rra(stack, false);
+	rrb(stack, false);
+	ft_printf("rrr\n");
+}
